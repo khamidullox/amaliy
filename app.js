a1662173827423b66d6a1de87e58084857ef054c
khamidullox <hamidullo22022@gmail.com>
2024-02-26 14:43:26 +0500

    amaliy mashiq

---
+++ app.js
@@ -0,0 +1,48 @@
+// let ism = prompt('Ismingizni kiriting')
+// let yosh = +prompt('Yosh kiriting')
+// let result = `Hurmatli ${ism} . Siz ${2024 - yosh} yilda tugilgansiz . Siz tugilganizdan ${yosh * 12 } oy , ${Math.trunc((yosh * 365)/7)} hafta , ${yosh * 365} kun , ${yosh * 365 *24} soat , ${yosh * 365 *24 *60} daqiqa otgan `
+// alert(result)
+
+// let ismlar = prompt("Isim kiriting");
+// let a = ["Hamidullo", "Begzod", "Timur"];
+// if (a.includes(ismlar)) {
+//   alert("Siz togri isim toptingiz");
+// }
+// alert("Siz notogri isim kiritingiz"
+
+// let ismlar = ["Hamidullo", "Begzod", "Timur"];
+// let result;
+// let i = 0;
+
+// while (ismlar.length > i) {
+//   result = ismlar[i] + "bek";
+//   i++;
+//   console.log(result);
+// }
+
+// let meyor = 20;
+// let elementalr = prompt("20 ta element yozing").length;
+// if (elementalr == 20) {
+//   alert(`Siz togti miqdorada elemet kiritingiz `);
+// } else if (elementalr < 20) {
+//   alert(`Siz ${meyor - elementalr} taga kam element kiritingiz`);
+// } else {
+//   alert(`Siz ${elementalr - meyor} taga kop element kiritingiz`);
+// }
+// let i = 0;
+// while (i <= 10) {
+//   let result = 9 * i;
+//   console.log(` 9 x ${i} = ${result}`);
+//   i++;
+// }
+
+// let word = "samarqand";
+// i = 0;
+// arr = word.split();
+
+// while (i < word.length) {
+//   let result = arr[i];
+
+//   i++;
+//   console.log(result);
+// }
